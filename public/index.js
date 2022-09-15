b44e54bca825e9cbc17ae48ddcc3852a4c08654e
Haunting2304 <60801196+Haunting2304@users.noreply.github.com>
2022-09-15 09:53:06 -0700

    Update index.js

--- public/index.js
+++ public/index.js
@@ -1,1 +1,13 @@
-
+document.querySelector("body").style.marginTop = `${document.querySelector("nav").offsetHeight}px`
+document.querySelector("body").style.minHeight = `calc(100vh - ${(document.querySelector("nav").offsetHeight)}px - 6em)`;
+window.onresize = style;
+function style() {
+    var nav = document.querySelector("nav");
+    var navItems = document.querySelectorAll(".navItem");
+    var body = document.querySelector("body");
+    var navItemMaxWidth = 250;
+    nav.style.width = `${body.clientWidth}px`;
+    nav.style.left = `calc(50% - ${nav.offsetWidth / 2}px)`;
+    navItems[i].style.left = `${i * navItemMaxWidth}px`;
+}
+style()
