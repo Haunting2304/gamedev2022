8f101e5c729abe01d4594aeabeac98272d96a145
Haunting2304 <60801196+Haunting2304@users.noreply.github.com>
2022-09-16 14:02:42 -0700

    Update index.js

--- public/index.js
+++ public/index.js
@@ -1,4 +1,4 @@
-document.querySelector("body").style.paddingTop = `${document.getElementById("repo").offsetHeight - window.getComputedStyle(document.querySelector("body"), null).getPropertyValue('padding-top') - window.getComputedStyle(document.querySelector("body"), null).getPropertyValue('padding-bottom')}px`;
+document.querySelector("body").style.paddingTop = `${document.getElementById("repo").offsetHeight - parseInt(window.getComputedStyle(document.querySelector("body"), null).getPropertyValue('padding-top')) - parseInt(window.getComputedStyle(document.querySelector("body"), null).getPropertyValue('padding-bottom'))}px`;
 document.querySelector("body").style.minHeight = `100vh`;
 window.onresize = style;
 function style() {
