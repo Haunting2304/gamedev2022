cf1c6a45f966a865c0ed7d52c99aff4cb7173f2a
Haunting2304 <60801196+Haunting2304@users.noreply.github.com>
2022-09-22 10:36:17 -0700

    Update backButton.js

--- public/backButton.js
+++ public/backButton.js
@@ -1,7 +1,7 @@
-document.querySelector("body").style.paddingTop = `${parseInt(window.getComputedStyle(document.querySelector("body"), null).getPropertyValue('padding-top')) + document.getElementById("repo").clientHeight}px`;
-document.querySelector("body").style.minHeight = `calc(100vh - ${parseInt(window.getComputedStyle(document.querySelector("body"), null).getPropertyValue('padding-top')) + parseInt(window.getComputedStyle(document.querySelector("body"), null).getPropertyValue('padding-bottom'))}px)`;
 var bodyHTML = document.querySelector("body").innerHTML;
 document.querySelector("body").innerHTML = `<div id="repo"><a href="https://github.com/Haunting2304/gamedev2022" class="link">← Repository (Giorgio)</a></div>` + bodyHTML
+document.querySelector("body").style.paddingTop = `${parseInt(window.getComputedStyle(document.querySelector("body"), null).getPropertyValue('padding-top')) + document.getElementById("repo").clientHeight}px`;
+document.querySelector("body").style.minHeight = `calc(100vh - ${parseInt(window.getComputedStyle(document.querySelector("body"), null).getPropertyValue('padding-top')) + parseInt(window.getComputedStyle(document.querySelector("body"), null).getPropertyValue('padding-bottom'))}px)`;
 window.onresize = style;
 function style() {
     var repo = document.getElementById("repo");
