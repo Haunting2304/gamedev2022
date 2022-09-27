838c094623d84389e15e5ead7ef52b4db7d10d85
Haunting2304 <60801196+Haunting2304@users.noreply.github.com>
2022-09-27 09:40:54 -0700

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,16 +1,10 @@
 var darkMode = true;
 debugger;
 if(darkMode == true) {
-    var a = document.createElement(link);
-    a.rel="stylesheet";
-    a.type="text/css";
-    a.href="/gamedev2022/public/style.css";
+    document.getElementById("styleMode").href="/gamedev2022/public/style.css";
 }
 else {
-    var a = document.createElement(link);
-    a.rel="stylesheet";
-    a.type="text/css";
-    a.href="/gamedev2022/public/styleLight.css";
+    document.getElementById("styleMode").href="/gamedev2022/public/styleLight.css";
 }
 window.onresize = style;
 function style() {
