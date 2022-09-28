b18382d7fdc30b9ea8c8c01b460a06b09ebf389c
Haunting2304 <60801196+Haunting2304@users.noreply.github.com>
2022-09-27 19:53:12 -0700

    Puuuush

--- stylePref.js
+++ stylePref.js
@@ -1,7 +1,3 @@
-window.onresize = style;
-function style() {
-}
-
 function stringToBool(x) {
     if(x == "true") {
         return true
@@ -19,7 +15,7 @@ if(localStorage.getItem('darkModePref') == undefined) {
 }
 var darkMode = stringToBool(localStorage.getItem('darkModePref'))
 if(darkMode) {
-    document.getElementById("styleMode").href="/gamedev2022/style.css";
+    document.getElementById("styleMode").href="/gamedev2022/styleDark.css";
 }
 else {
     document.getElementById("styleMode").href="/gamedev2022/styleLight.css";
@@ -28,13 +24,10 @@ function toggleDarkMode() {
     darkMode = !darkMode
     if(darkMode) {
         localStorage.setItem('darkModePref', true);
-        document.getElementById("styleMode").href="/gamedev2022/style.css";
+        document.getElementById("styleMode").href="/gamedev2022/styleDark.css";
     }
     else {
         localStorage.setItem('darkModePref', false);
         document.getElementById("styleMode").href="/gamedev2022/styleLight.css";
     }
 }
-
-
-style()
