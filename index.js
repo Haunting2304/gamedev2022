156ef054c0242255185ee13f03eb814b07d263fd
Haunting2304 <60801196+Haunting2304@users.noreply.github.com>
2022-09-29 14:16:14 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ var settingsOpen = false;
 function toggleSettings() {
     settingsOpen = !settingsOpen
     if(settingsOpen) {
-        document.getElementById("settingsMenu").style.visibility = "visible"
+        document.getElementById("settingsMenu").hidden = false
     }
     else {
-        document.getElementById("settingsMenu").style.visibility = "hidden"
+        document.getElementById("settingsMenu").hidden = true
     }
 }
