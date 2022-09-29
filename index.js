98af39d4f6edc296e461e4c9e770cedaae55f6df
Haunting2304 <60801196+Haunting2304@users.noreply.github.com>
2022-09-29 14:10:21 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-
+var settingsOpen = false;
+function toggleSettings() {
+    settingsOpen = !settingsOpen
+    if(settingsOpen) {
+        console.log("Open Settings")
+    }
+    else {
+        console.log("Close Settings")
+    }
+}
