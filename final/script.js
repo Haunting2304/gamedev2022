47b6d402498f43c4e2b802c171b0a9bbadf1b207
Haunting2304 <60801196+Haunting2304@users.noreply.github.com>
2023-01-05 10:04:06 -0800

    Add files via upload

--- final/script.js
+++ final/script.js
@@ -10,12 +10,17 @@ var drawWidth
 var drawHeight
 var xOffset
 var yOffset
+var cameraX = 0
+var cameraY = 0
+var keyPressed = {}
 var canvasElement = document.querySelector('canvas');
 var canvasContext = canvasElement.getContext('2d');
 // canvasContext.lineCap = 'round' //Doesn't work for some reason
 var mouse = {
     x:0,
-    y:0
+    y:0,
+    internalX:function(){return toInternalX(this.x)},
+    internalY:function(){return toInternalY(this.y)}
 }
 var itemsList = {}
 var drawList = []
@@ -38,20 +43,26 @@ function deleteID(id) {
 function toCanvasX(input) {
     return (input * drawWidth / 1000 / aspectRatio)
 }
-function toCanvasY(input) { //Maybe flip so the internal coordinates work how I want
+function toCanvasY(input) { //Maybe flip the sign so the internal coordinates work how I want
     return (input * drawHeight / 1000)
 }
 function toDrawX(input) {
-    return (input * drawWidth / 1000 / aspectRatio)+xOffset
+    return (input * drawWidth / 1000 / aspectRatio)+xOffset-toCanvasX(cameraX)
 }
 function toDrawY(input) {
-    return (input * drawHeight / 1000)+yOffset
+    return (input * drawHeight / 1000)+yOffset-toCanvasY(cameraY)
 }
-function toInternalX(input) { //these functions suck to look at so probably do something about it
-    return (input-xOffset) / drawWidth * 1000 * aspectRatio
+function toInternalX(input) { //these functions suck to look at so probably do something about it //only use if input has the offset applied already
+    return (input-xOffset+toCanvasX(cameraX)) / drawWidth * 1000 * aspectRatio
 }
 function toInternalY(input) {
-    return (input-yOffset) / drawHeight * 1000
+    return (input-yOffset+toCanvasY(cameraY)) / drawHeight * 1000
+}
+function toInternalnX(input) { //these functions suck to look at so probably do something about it //only use if input has the offset applied already
+    return (input) / drawWidth * 1000 * aspectRatio
+}
+function toInternalnY(input) {
+    return (input) / drawHeight * 1000
 }
 function updateDrawList() {
     drawList = []
@@ -62,8 +73,10 @@ function updateDrawList() {
     drawList.sort((a,b)=>{return a.z-b.z})
 }
 function drawCanvas() {
-    canvasContext.clearRect(0, 0, canvasElement.width, canvasElement.height)
+    // canvasContext.clearRect(0, 0, canvasElement.width, canvasElement.height)
     //Temp for testing
+    canvasContext.lineWidth = 1
+    canvasContext.strokeStyle = '#222'
     canvasContext.fillStyle = '#222'
     canvasContext.fillRect(0, 0, canvasElement.width, canvasElement.height)
     for(let i=0; i<drawList.length; i++) {
@@ -72,6 +85,7 @@ function drawCanvas() {
         canvasContext.lineWidth = item.lineWidth !== undefined ? item.lineWidth : 1
         canvasContext.strokeStyle = item.color !== undefined ? item.color : '#000'
         canvasContext.fillStyle = item.fillColor !== undefined ? item.fillColor : canvasContext.strokeStyle
+        canvasContext.setLineDash(item.lineDash !== undefined ? item.lineDash : [])
         switch(item.type) {
             case 'rect':
                 canvasContext.beginPath()
@@ -86,6 +100,16 @@ function drawCanvas() {
                 canvasContext.moveTo(toDrawX(item.x), toDrawY(item.y))
                 canvasContext.lineTo(toDrawX(item.x2), toDrawY(item.y2))
                 canvasContext.stroke()
+                break
+            case 'circle':
+                canvasContext.beginPath()
+                canvasContext.arc(toDrawX(item.x), toDrawY(item.y), toCanvasX(item.radius), item.startAngle, item.endAngle)
+                canvasContext.stroke()
+                break
+            case 'fillCircle':
+                canvasContext.beginPath()
+                canvasContext.arc(toDrawX(item.x), toDrawY(item.y), toCanvasX(item.radius), item.startAngle, item.endAngle)
+                canvasContext.fill()
                 break
             case 'image':
                 canvasContext.drawImage(item.image, toDrawX(item.x), toDrawY(item.y), toCanvasX(item.width), toCanvasY(item.height))
@@ -133,34 +157,47 @@ function updateCanvasSize() {
 updateCanvasSize()
 window.addEventListener('resize', updateCanvasSize)
 window.addEventListener('mousemove', (event)=>{
-    if(event.clientX < xOffset) {
-        mouse.x = toInternalX(xOffset)
+    if(event.clientX < xOffset) { //Bounds for mouse position
+        mouse.x = xOffset
     }
     else if(event.clientX > drawWidth+xOffset) {
-        mouse.x = toInternalX(drawWidth+xOffset)
+        mouse.x = drawWidth+xOffset
     }
     else {
-        mouse.x = toInternalX(event.clientX / parseInt(getComputedStyle(canvasElement).width) * canvasElement.width)
+        mouse.x = event.clientX / parseInt(getComputedStyle(canvasElement).width) * canvasElement.width
     }
     if(event.clientY < yOffset) {
-        mouse.y = toInternalY(yOffset)
+        mouse.y = yOffset
     }
     else if(event.clientY > drawHeight+yOffset) {
-        mouse.y = toInternalY(drawHeight+yOffset)
+        mouse.y = drawHeight+yOffset
     }
     else {
-        mouse.y = toInternalY(event.clientY / parseInt(getComputedStyle(canvasElement).height) * canvasElement.height)
+        mouse.y = event.clientY / parseInt(getComputedStyle(canvasElement).height) * canvasElement.height
     }
 })
 
+document.addEventListener("keydown", (e)=>{
+    e = e || window.event;
+    keyPressed[e.code] = true
+});
 
-function createItem(arguments, bypass) {
+document.addEventListener("keyup", (e)=>{
+    e = e || window.event;
+    delete keyPressed[e.code]
+});
+
+window.addEventListener("blur", ()=>{
+    keyPressed = {}
+});
+
+function createItem(arguments) {
     let id = createID()
     itemsList[id] = {}
     for(let i in arguments) {
         itemsList[id][i] = arguments[i]
     }
-    updateDrawList() //May needlessly tank performance if making multiple new items in one frame because of excessive sorting
+    updateDrawList() //May needlessly tank performance if making many new items in one frame because of excessive sorting
 }
 function removeItem(id) {
     delete itemsList[id]
@@ -168,28 +205,90 @@ function removeItem(id) {
     updateDrawList()
 }
 createItem({
-    type:'rect',
-    x:10,
-    y:10,
-    update:function(){
-        this.x2 = mouse.x
-        this.y2 = mouse.y
-        this.width = this.x2 - this.x
-        this.height = this.y2 - this.y
-    },
-    color:'#f00',
-    lineWidth:'5'
+    type:'line',
+    x:-10000,
+    y:0,
+    x2:10000,
+    y2:0,
+    lineDash:[15,5],
+    z:-100,
+    lineWidth:'5',
+    color:'#f00'
 })
 createItem({
     type:'line',
-    x:10,
-    y:10,
+    x:0,
+    y:-10000,
+    x2:0,
+    y2:10000,
+    lineDash:[15,5],
+    z:-100,
+    lineWidth:'5',
+    color:'#0f0'
+})
+// createItem({
+//     type:'rect',
+//     x:10,
+//     y:10,
+//     update:function(){
+//         this.x2 = mouse.internalX()
+//         this.y2 = mouse.internalY()
+//         this.width = this.x2 - this.x
+//         this.height = this.y2 - this.y
+//     },
+//     color:'#f00',
+//     lineWidth:'5'
+// })
+// createItem({
+//     type:'line',
+//     x:10,
+//     y:10,
+//     update:function(){
+//         this.x2 = mouse.internalX()
+//         this.y2 = mouse.internalY()
+//     },
+//     z:-1,
+//     lineWidth:'10',
+// })
+createItem({
+    type:'fillCircle',
     update:function(){
-        this.x2 = mouse.x
-        this.y2 = mouse.y
+        this.x = mouse.internalX()
+        this.y = mouse.internalY()
     },
-    z:-1,
-    lineWidth:'10'
+    radius:5,
+    startAngle:0,
+    endAngle:2*Math.PI,
+    color:'#f00',
+    fillColor:this.color,
+    z:100
+})
+createItem({
+    type:'fillCircle',
+    x:100,
+    y:100,
+    update:function(){
+        if(keyPressed.KeyW) {
+            this.y -= 5
+        }
+        if(keyPressed.KeyA) {
+            this.x -= 5
+        }
+        if(keyPressed.KeyS) {
+            this.y += 5
+        }
+        if(keyPressed.KeyD) {
+            this.x += 5
+        }
+        cameraX = this.x - toInternalnX(canvasElement.width) / 2
+        cameraY = this.y - toInternalnY(canvasElement.height) / 2
+    },
+    radius:15,
+    startAngle:0,
+    endAngle:2*Math.PI,
+    color:'#f00',
+    fillColor:this.color,
+    z:100
 })
 // itemsList[createID()] = {
 //     type:'rect',
@@ -244,10 +343,11 @@ createItem({
     y:100,
     z:-5,
     onSpawn:function(){
-        let image = createImage({src:'TestImage.png'}) //eg. code
+        let image = createImage({src:'TestImage.png'}) //Idk why I can't use this.image instead //eg. code
         let ratio = image.naturalWidth/image.naturalHeight
         this.width = 500
-        this.height = 500
+        this.height = this.width * ratio
+        this.width = this.height / ratio
     }
 })
 function getAvgFPS() {
@@ -266,6 +366,18 @@ function getAvgFPS() {
 }
 function frameUpdate() {
     now = Date.now()
+    // if(keyPressed.KeyW) {
+    //     cameraY -= 5
+    // }
+    // if(keyPressed.KeyA) {
+    //     cameraX -= 5
+    // }
+    // if(keyPressed.KeyS) {
+    //     cameraY += 5
+    // }
+    // if(keyPressed.KeyD) {
+    //     cameraX += 5
+    // }
     let elapsed = now - then
     FPSCounter[FPSCounter.length] = {delay:1000/elapsed, time:now}
     disp1 = `Avg FPS: ${Math.floor(getAvgFPS())}`
